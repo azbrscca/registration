1321709f277855beb801ee4e22b6ff4684dd36db
Jeff Israel <jeff.israel@gmail.com>
2019-12-28 20:13:09 -0600

    Adding configure script

---
+++ configure.php
@@ -0,0 +1,37 @@
+<?php
+  $dir = getcwd();
+  echo $dir."\n";
+
+  $targets = array( "dev", "live" );
+
+  if ( ( sizeof( $argv ) < 3 ) ||
+       !in_array( strtolower( $argv[ 1 ] ), $targets ) )  {
+    echo "Usage: php configure.php (dev|live) (configuration directory location)\n -v";
+    exit;
+  }
+
+  $target = strtolower( $argv[ 1 ] );
+  $configDir = $argv[ 2 ];
+  $verbose = ( ( sizeof( $argv ) > 3 ) && ( $argv[ 3 ] == '-v' ) );
+
+  $files = array(
+    "common/Common.php",
+    "db/ConnectionFactory.php",
+  );
+
+  foreach( $files as $dst ) {
+
+    $src = $configDir."/".$dst.".".$target;
+    if ( file_exists( $src ) ) {
+      if ( $verbose ) { echo "copying: ".$src." to ".$dst."\n"; }
+      if ( copy( $src, $dst ) ) {
+        if ( $verbose ) { echo "done\n"; }
+      }
+    }
+  }
+
+  if ( $verbose ) { echo "deleting: ".$dir."/".$_SERVER[ 'SCRIPT_NAME' ]."\n"; }
+  unlink( $dir."/".$_SERVER[ 'SCRIPT_NAME' ] );
+  unlink( $dir."/README.md" );
+  echo "done\n";
+?>
