6d1f2ffddd6c3467085305b19611ec32047fb8a7
Manfred Reysser <mreysser@gmail.com>
2020-10-19 19:02:59 -0700

    Add waitlist entry position logic

--- common/DatabaseHelper.php
+++ common/DatabaseHelper.php
@@ -9,6 +9,22 @@
       $registered = ( $q->count() == 1 );
       return $registered;
     }
+    
+    public static function getEntrantRegPosition( $entrant_id, $event_id ) {
+      $q = new Query( "registrations" );
+      $q->addWhere( 'event_id', $event_id );
+      $event_registrations = $q->select();
+      
+      $idx = 1;
+      foreach ( $event_registrations as $row ) {
+        if ( $row[ 'entrant_id' ] == $entrant_id ) {
+          return $idx;
+        }
+        $idx += 1;
+      }
+      
+      return 0;
+    }
 
   } // class DatabaseHelper
 ?>
